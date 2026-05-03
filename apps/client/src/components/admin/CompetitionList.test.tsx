42819a01e8887edd4828a2891f8420357d4f091f
Atanas Kaynarov <akaynarov@gmail.com>
2026-05-03 15:16:18 +0300

    refactor: remove unnecessary React import from CompetitionList test

--- apps/client/src/components/admin/CompetitionList.test.tsx
+++ apps/client/src/components/admin/CompetitionList.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { describe, expect, it, vi } from "vitest";
 import { render, click } from "../../test/render";
 import { CompetitionList } from "./CompetitionList";
